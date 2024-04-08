cbe4a45bb56bf003cdd38d4b2f799495c221849c
Ryan Willyanto <ryanwillyanto@gmail.com>
2024-04-08 07:58:05 +0700

    feat: Added Navbar Logout

--- client/src/components/RightNavBar.tsx
+++ client/src/components/RightNavBar.tsx
@@ -1,4 +1,46 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+
+const url = import.meta.env.VITE_BASE_URL || "http://localhost:3000";
+
 const RightNavBar = () => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  const handleLogout = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(url + "/logout", {
+        method: "GET",
+        credentials: "include",
+        signal,
+      });
+      if (response.ok) {
+        navigate("/login");
+      } else {
+        const responseJson = await response.json();
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: responseJson.error,
+        });
+      }
+      setIsLoading(false);
+    } catch (error: any) {
+      setIsLoading(false);
+      console.error(error);
+
+      if (error.name !== "Abort Error")
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something is wrong",
+        });
+    }
+  };
   return (
     <div className="flex w-1/3 items-center gap-4 justify-end">
       {/* Personal Info */}
@@ -29,17 +71,24 @@ const RightNavBar = () => {
       </div>
 
       {/* Logout */}
-      <div className=" bg-slate-200 w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="#7ed321"
-        >
-          <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4M10 17l5-5-5-5M13.8 12H3" />
-        </svg>
+      <div
+        className=" bg-slate-200 w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+        onClick={handleLogout}
+      >
+        {isLoading ? (
+          <span className="loading loading-spinner loading-xs"></span>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#7ed321"
+          >
+            <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4M10 17l5-5-5-5M13.8 12H3" />
+          </svg>
+        )}
       </div>
     </div>
   );
